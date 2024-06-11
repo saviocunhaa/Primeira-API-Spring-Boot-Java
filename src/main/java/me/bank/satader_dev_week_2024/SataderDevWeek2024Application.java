6874ac074e5a9e55f7597b5abfef875b794aeefb
saviocunhaa <saviocunha.01@hotmail.com>
2024-06-11 10:00:19 -0300

    Iniciando Projeto Spring Boot

---
+++ src/main/java/me/bank/satader_dev_week_2024/SataderDevWeek2024Application.java
@@ -0,0 +1,13 @@
+package me.bank.satader_dev_week_2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SataderDevWeek2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SataderDevWeek2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/bank/satader_dev_week_2024/SataderDevWeek2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bank.satader_dev_week_2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SataderDevWeek2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
